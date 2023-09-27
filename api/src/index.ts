8793dd632356665d54e8674b3b0025595fbf8eff
galamo <galamouyal88@gmail.com>
2023-09-27 20:26:37 +0300

    a

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,7 +2,7 @@ import express, { Request } from "express"
 import dotenv from "dotenv"
 import { initConnection } from "./db/connection"
 import { VehiclesModel } from "./db/models/vehicles"
-import { TestModel } from "./db/models/test"
+
 dotenv.config()
 initConnection()
 const app = express();
@@ -11,7 +11,11 @@ app.get("/health-check", (req, res) => {
     res.json({ message: "API IS RUNNING" })
 })
 app.get("/vehicles", async (req: Request, res, next) => {
-    const result = await VehiclesModel.find({});
+    const query = req.query
+    const { hp, acc } = query
+    const hpCriteria = hp ? { Horsepower: { $gt: Number(hp) } } : {}
+    const accCriteria = acc ? { Acceleration: { $gt: Number(acc) } } : {}
+    const result = await VehiclesModel.find({ ...hpCriteria, ...accCriteria });
     res.json(result)
 })
 
