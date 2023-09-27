71922b8d4e24d430a673990b86b4726f891b7855
galamo <galamouyal88@gmail.com>
2023-09-27 21:22:56 +0300

    a

--- api/readme.md
+++ api/readme.md
@@ -1,3 +1,32 @@
-# EX 
+# EX  1
 1. Support veicle Weight_in_lbs filter
 2. Support number of result, size
+
+
+# EX  2
+1. Add new field - manufacturer 
+2. Create aggregation query for Manufacturer
+
+
+
+
+# Homework
+ 1. Api - vehicles
+ 2. Client - Angular
+ 3. DB - mongoDB
+
+ - Create Angular client with the following pages :
+ - vehicles - presenting the all vehicles with filters
+ - add vehicles - will support adding new vehicle to the collection |( form with all the fields)
+ - update vehicle price , search vehicle by ID, the page will be empty and show result only if vehicle exist, add a number input to update the price , also insert into the input the old price if exist, save the new price by clicking the save button
+- save every update price operation in audit collection ( new collection ) that holds the car id, new price, old price and timestamp
+- show the audit in a new page ( table )
+
+Important notes:
+ 1. create 4 routes
+ 2. show message when operation succeeded
+ 3. support reusing the components ( car row, table )
+
+
+
+
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,4 +1,5 @@
 import express, { Request } from "express"
+import bodyParser from "body-parser"
 import dotenv from "dotenv"
 import { initConnection } from "./db/connection"
 import { VehiclesModel } from "./db/models/vehicles"
@@ -6,18 +7,35 @@ import { VehiclesModel } from "./db/models/vehicles"
 dotenv.config()
 initConnection()
 const app = express();
-
+app.use(bodyParser())
 app.get("/health-check", (req, res) => {
     res.json({ message: "API IS RUNNING" })
 })
 app.get("/vehicles", async (req: Request, res, next) => {
     const query = req.query
-    const { hp, acc } = query
+    const { hp, acc, weight, size = 100 } = query
     const hpCriteria = hp ? { Horsepower: { $gt: Number(hp) } } : {}
     const accCriteria = acc ? { Acceleration: { $gt: Number(acc) } } : {}
-    const result = await VehiclesModel.find({ ...hpCriteria, ...accCriteria });
+    const weightCriteria = weight ? { Weight_in_lbs: { $gt: Number(weight) } } : {}
+    const limit = !isNaN(Number(size)) && Number(size) > 200 ? 100 : Number(size)
+    const result = await VehiclesModel.find({ ...hpCriteria, ...accCriteria, ...weightCriteria }, { Horsepower: 1 }).limit(limit as number).sort({ Horsepower: 1 });
     res.json(result)
 })
+
+app.get("/origins", async (req: Request, res, next) => {
+    const result = await VehiclesModel.aggregate([{ $group: { _id: "$Origin", count: { $sum: 1 } } }])
+    res.json(result)
+})
+
+app.post("/vehicles", async (req: Request, res, next) => {
+    const body = req.body
+    console.log(body)
+    const model = new VehiclesModel(body)
+    await model.save()
+    res.json({ message: body })
+})
+
+
 
 
 app.listen(process.env.PORT, () => {
